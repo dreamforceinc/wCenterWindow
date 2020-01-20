81510d510571a183f3ae89a544a142325962fb2a
dreamforceinc <dreamforce@yandex.ru>
2020-01-20 07:01:00 +0300

    fix files

--- wCenterWindow/framework.h
+++ wCenterWindow/framework.h
@@ -8,6 +8,8 @@
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
 #include <windows.h>
+#include <strsafe.h>
+#include <shellapi.h>
 // Файлы заголовков среды выполнения C
 #include <stdlib.h>
 #include <malloc.h>
--- wCenterWindow/wCenterWindow.cpp
+++ wCenterWindow/wCenterWindow.cpp
@@ -4,177 +4,207 @@
 #include "framework.h"
 #include "wCenterWindow.h"
 
-#define MAX_LOADSTRING 100
+#define MAX_LOADSTRING 80
+#define WM_WCW	WM_USER + 0x7F00
 
 // Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
-WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+HINSTANCE		hInst;						// Текущий экземпляр
+WCHAR			szTitle[MAX_LOADSTRING];	// Текст строки заголовка
+WCHAR			szClass[MAX_LOADSTRING];	// Имя класса главного окна
+WCHAR			szHelp[MAX_LOADSTRING];		// Текст описания
+HHOOK			KeyboardHook;
+HICON			hIcon;
+HMENU			hMenu, hPopup;
+HWND			hWnd;
+NOTIFYICONDATA	nid = { 0 };
+KBDLLHOOKSTRUCT	*pkhs;
+int				dtCenterX, dtCenterY;
+bool			pressed = FALSE;
 
-// Отправить объявления функций, включенных в этот модуль кода:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+// Прототипы функций
+ATOM				MyRegisterClass(HINSTANCE);
+VOID				ShowError(HINSTANCE, UINT);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK	KeyboardHookProc(int, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+// Точка входа
+int APIENTRY wWinMain(_In_		HINSTANCE	hInstance,
+					  _In_opt_	HINSTANCE	hPrevInstance,
+					  _In_		LPWSTR		lpCmdLine,
+					  _In_		int			nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Разместите код здесь.
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDS_CLASSNAME, szClass, MAX_LOADSTRING);
+	if (FindWindow(szClass, NULL))
+	{
+		ShowError(hInstance, IDS_RUNNING);
+		return FALSE;
+	}
+	MyRegisterClass(hInstance);
+	hWnd = CreateWindowExW(0, szClass, szTitle, 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
+	if (!hWnd)
+	{
+		ShowError(hInstance, IDS_ERR_WND);
+		return FALSE;
+	}
 
-    // Инициализация глобальных строк
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WCENTERWINDOW, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	nid.cbSize = sizeof(NOTIFYICONDATA);
+	nid.hWnd = hWnd;
+	nid.uVersion = NOTIFYICON_VERSION;
+	nid.uCallbackMessage = WM_WCW;
+	nid.hIcon = hIcon;
+	nid.uID = IDI_TRAYICON;
+	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	nid.dwInfoFlags = NIIF_INFO;
+	StringCchCopy(nid.szTip, sizeof(nid.szTip), szTitle);
+	if (!Shell_NotifyIcon(NIM_ADD, &nid))
+	{
+		ShowError(hInstance, IDS_ERR_ICON);
+		SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+	}
 
-    // Выполнить инициализацию приложения:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU));
+	if (!hMenu)
+	{
+		ShowError(hInstance, IDS_ERR_MENU);
+		SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WCENTERWINDOW));
+	hPopup = GetSubMenu(hMenu, 0);
+	if (!hPopup)
+	{
+		ShowError(hInstance, IDS_ERR_POPUP);
+		SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+	}
 
-    MSG msg;
+	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, hInstance, NULL);
+	if (!KeyboardHook)
+	{
+		ShowError(hInstance, IDS_ERR_HOOK);
+		SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+	}
 
-    // Цикл основного сообщения:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	LoadStringW(hInstance, IDS_HELPTEXT, szHelp, MAX_LOADSTRING);
+	RECT dtrc = { 0 };
+	SystemParametersInfo(SPI_GETWORKAREA, NULL, &dtrc, FALSE);
+	dtCenterX = dtrc.right / 2, dtCenterY = dtrc.bottom / 2;
 
-    return (int) msg.wParam;
+	MSG msg;
+	BOOL bRet;
+
+	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
+	{
+		if (bRet == -1)
+		{
+			ShowError(hInstance, IDS_ERR_MAIN);
+			return -1;
+		}
+		else
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
 }
 
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  ЦЕЛЬ: Регистрирует класс окна.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WCENTERWINDOW));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WCENTERWINDOW);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TRAYICON));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.lpszClassName = szClass;
+	wcex.hIconSm = wcex.hIcon;
+	hIcon = wcex.hIcon;
+	return RegisterClassEx(&wcex);
 }
 
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
-//
-//   КОММЕНТАРИИ:
-//
-//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится главное окно программы.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND  - обработать меню приложения
-//  WM_PAINT    - Отрисовка главного окна
-//  WM_DESTROY  - отправить сообщение о выходе и вернуться
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Разобрать выбор в меню:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_WCW:
+		if (lParam == WM_RBUTTONDOWN && wParam == IDI_TRAYICON)
+		{
+			SetForegroundWindow(hWnd);
+			POINT pt;
+			GetCursorPos(&pt);
+			int idMenu = TrackPopupMenu(hPopup, TPM_RETURNCMD, pt.x, pt.y, 0, hWnd, NULL);
+			if (idMenu == ID_POPUPMENU_HELP && !pressed)
+			{
+				pressed = TRUE;
+				if (MessageBox(hWnd, szHelp, szTitle, MB_OK | MB_TOPMOST) == IDOK) pressed = FALSE;
+			}
+			if (idMenu == ID_POPUPMENU_EXIT) SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+		}
+		break;
+
+	case WM_DESTROY:
+		if (KeyboardHook) UnhookWindowsHookEx(KeyboardHook);
+		if (hMenu) DestroyMenu(hMenu);
+		Shell_NotifyIcon(NIM_DELETE, &nid);
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	pkhs = (KBDLLHOOKSTRUCT*)lParam;
+	bool bLCTRL, bLWIN;
+	GetAsyncKeyState(VK_LCONTROL) < 0 ? bLCTRL = TRUE : bLCTRL = FALSE;
+	GetAsyncKeyState(VK_LWIN) < 0 ? bLWIN = TRUE : bLWIN = FALSE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	if ((bLCTRL && bLWIN && pkhs->vkCode == 0x43) && !pressed)
+	{
+		pressed = TRUE;
+		HWND fgWindow = GetForegroundWindow();
+		if (fgWindow)
+		{
+			HWND parentWindow = fgWindow;
+			while (TRUE)
+			{
+				parentWindow = GetParent(fgWindow);
+				if (parentWindow) fgWindow = parentWindow;
+				else break;
+			}
+
+			WINDOWPLACEMENT wp = { 0 };
+			wp.length = sizeof(WINDOWPLACEMENT);
+			GetWindowPlacement(fgWindow, &wp);
+			if (wp.showCmd == SW_SHOWNORMAL)
+			{
+				int fgW = wp.rcNormalPosition.right - wp.rcNormalPosition.left;
+				int fgH = wp.rcNormalPosition.bottom - wp.rcNormalPosition.top;
+				int fgX = dtCenterX - (fgW / 2);
+				int fgY = dtCenterY - (fgH / 2);
+				wp.rcNormalPosition.left = fgX;
+				wp.rcNormalPosition.top = fgY;
+				wp.rcNormalPosition.right = fgX + fgW;
+				wp.rcNormalPosition.bottom = fgY + fgH;
+				SendMessage(fgWindow, WM_ENTERSIZEMOVE, NULL, NULL);
+				SetWindowPlacement(fgWindow, &wp);
+				SendMessage(fgWindow, WM_EXITSIZEMOVE, NULL, NULL);
+			}
+		}
+		pressed = FALSE;
+	}
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+VOID ShowError(HINSTANCE hInstance, UINT uID)
+{
+	WCHAR szErrorText[MAX_LOADSTRING]; // Текст ошибки
+	LoadStringW(hInstance, uID, szErrorText, MAX_LOADSTRING);
+	MessageBox(hWnd, szErrorText, szTitle, MB_OK | MB_ICONERROR);
 }

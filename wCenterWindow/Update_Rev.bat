a2561847374a76f6fcb33534cf314937831eb224
W0LF <dreamforce@yandex.ru>
2022-03-14 18:24:48 +0300

    Fixed Update_Rev.bat

--- wCenterWindow/Update_Rev.bat
+++ wCenterWindow/Update_Rev.bat
@@ -3,13 +3,20 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET CURRENT_TIME=%TIME%
 SET CURRENT_DATE=%DATE%
-SET CURRENT_YEAR=%CURRENT_DATE:~10,4%
 
-SET BUILDTIME=0
-SET GIT_BRANCH=LOCAL
-SET GIT_REV_COUNT=0
+ECHO #pragma once > revision.h
+
+SET BUILDTIME=%CURRENT_TIME:~0,8%
+SET BUILDDATE=%CURRENT_DATE%
+SET CURRENT_YEAR=%CURRENT_DATE:~6,4%
+
+SET BUILDSECS=0
+SET GIT_COUNT=0
+SET GIT_TIME=0
 SET GIT_DATE=0
-SET GIT_DATE_HUMAN=0
+SET GIT_DATE_NUM=0
+SET GIT_DATETIME=0
+SET GIT_BRANCH="LOCAL"
 
 SET VerMajor=0
 SET VerMinor=0
@@ -35,27 +42,21 @@ FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_NAME" "Version.h"') 
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_YEAR_START" "Version.h"') DO (SET "PYS=%%A")
 FOR /F "tokens=2*" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_AUTHORS" "Version.h"') DO (SET "PA=%%~B")
 
-FOR /F "tokens=1-4 delims=:.," %%A IN ("%CURRENT_TIME%") DO (
-  SET /A "BUILDTIME=(100%%A %% 100) * 3600 + (100%%B %% 100) * 60 + (100%%C %% 100)"
-)
+FOR /F "tokens=1-4 delims=:., " %%A IN ("%BUILDTIME%") DO (SET /A "BUILDSECS=%%A * 3600 + %%B * 60 + %%C")
 
 FOR /F "delims=" %%A IN ('git symbolic-ref --short HEAD') DO (SET GIT_BRANCH=%%A)
+FOR /F "delims=" %%A IN ('git rev-list --count HEAD') DO (SET /A GIT_COUNT=%%A)
 
-FOR /F "delims=" %%A IN ('git rev-list --count HEAD') DO (
-  SET /A GIT_REV_COUNT=%%A - 57
+::FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%y%%m%%d ^| find /I "Date:"') DO (SET GIT_DATE_NUM=%%B)
+FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%d.%%m.%%Y ^| find /I "Date:"') DO (SET "GIT_DATE=%%B")
+FOR /F "tokens=2-4 delims=, " %%A IN ('git log -1 --date=format:"%%a,%%d-%%h-%%Y,%%T" ^| find /I "Date:"') DO (
+  SET "GIT_DATETIME=Build time: %%A, %%B %%C"
+  SET "GIT_TIME=%%C"
 )
 
-FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%y%%m%%d ^| find /I "Date:"') DO (
-  SET GIT_DATE=%%B
-)
-
-FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%d.%%m.%%Y ^| find /I "Date:"') DO (
-  SET GIT_DATE_HUMAN=%%B
-)
-
-SET VSF=%VerMajor%.%VerMinor%.%GIT_DATE%.%BUILDTIME%
+SET VSF=%VerMajor%.%VerMinor%.%VerBuild%.%GIT_COUNT%
 SET VS=%VerMajor%.%VerMinor%.%VerBuild%
-SET VNF=%VerMajor%,%VerMinor%,%GIT_DATE%,%BUILDTIME%
+SET VNF=%VerMajor%,%VerMinor%,%VerBuild%,%GIT_COUNT%
 SET VN=%VerMajor%,%VerMinor%,%VerBuild%
 
 SET PNF="%PN% v%VS% (C++)"
@@ -64,12 +65,15 @@ SET PCF="Copyright (C) %PYS%-%CURRENT_YEAR% by %PA%"
 SET INT_NAME="%PN%C++"
 SET ORIG_NAME="%PN%.exe"
 
-ECHO #pragma once > revision.h
-ECHO #define GIT_DATE_HUMAN "%GIT_DATE_HUMAN%" >> revision.h
+ECHO #define BUILD_DATE "%BUILDDATE%" >> revision.h
+ECHO #define BUILD_TIME "%BUILDTIME%" >> revision.h
+::ECHO #define GIT_DATE_HUMAN "%GIT_DATE_NUM%" >> revision.h
 ECHO #define GIT_DATE "%GIT_DATE%" >> revision.h
-ECHO #define GIT_BRANCH %GIT_BRANCH% >> revision.h
-ECHO #define GIT_COUNT %GIT_REV_COUNT% >> revision.h
-ECHO #define V_SECS %BUILDTIME% >> revision.h
+ECHO #define GIT_TIME "%GIT_TIME%" >> revision.h
+ECHO #define GIT_DATETIME "%GIT_DATETIME%" >> revision.h
+ECHO #define GIT_BRANCH "%GIT_BRANCH%" >> revision.h
+ECHO #define GIT_COUNT %GIT_COUNT% >> revision.h
+ECHO #define V_SECS %BUILDSECS% >> revision.h
 ECHO #define INTERNAL_NAME %INT_NAME% >> revision.h
 ECHO #define ORIG_FILE_NAME %ORIG_NAME% >> revision.h
 ECHO #define PRODUCT_NAME_FULL %PNF% >> revision.h
@@ -78,6 +82,6 @@ ECHO #define VERSION_NUM %VN% >> revision.h
 ECHO #define VERSION_STR "%VS%" >> revision.h
 ECHO #define VERSION_NUM_FULL %VNF% >> revision.h
 ECHO #define VERSION_STR_FULL "%VSF%" >> revision.h
-
+::pause
 ENDLOCAL
 EXIT
--- wCenterWindow/revision.h
+++ wCenterWindow/revision.h
@@ -1,14 +1,17 @@
 #pragma once 
-#define GIT_DATE_HUMAN "02.03.2022" 
-#define GIT_DATE "220302" 
-#define GIT_BRANCH dev 
-#define GIT_COUNT 3 
-#define V_SECS 38575 
+#define BUILD_DATE "14.03.2022" 
+#define BUILD_TIME "17:58:14" 
+#define GIT_DATE "13.03.2022" 
+#define GIT_TIME "11:45:21" 
+#define GIT_DATETIME "Build time: Sun, 13-Mar-2022 11:45:21" 
+#define GIT_BRANCH "dev" 
+#define GIT_COUNT 61 
+#define V_SECS 64694 
 #define INTERNAL_NAME "wCenterWindowC++" 
 #define ORIG_FILE_NAME "wCenterWindow.exe" 
 #define PRODUCT_NAME_FULL "wCenterWindow v2.3.3 (C++)" 
 #define PRODUCT_COPYRIGHT "Copyright (C) 2020-2022 by W0LF aka 'dreamforce'" 
 #define VERSION_NUM 2,3,3 
 #define VERSION_STR "2.3.3" 
-#define VERSION_NUM_FULL 2,3,220302,38575 
-#define VERSION_STR_FULL "2.3.220302.38575" 
+#define VERSION_NUM_FULL 2,3,3,61 
+#define VERSION_STR_FULL "2.3.3.61" 

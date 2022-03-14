bcf45417d8365fc803e2eb25ab7dbd7e1c2bf4af
W0LF <dreamforce@yandex.ru>
2022-03-14 20:23:25 +0300

    Update Update_Rev.bat. File Update_Rev.bat moved outside the project folder.

--- Update_Rev.bat
+++ Update_Rev.bat
@@ -1,10 +1,8 @@
-@ECHO OFF
+::@ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET CURRENT_TIME=%TIME%
 SET CURRENT_DATE=%DATE%
-
-ECHO #pragma once > revision.h
 
 SET BUILDTIME=%CURRENT_TIME:~0,8%
 SET BUILDDATE=%CURRENT_DATE%
@@ -16,11 +14,12 @@ SET GIT_TIME=0
 SET GIT_DATE=0
 SET GIT_DATE_NUM=0
 SET GIT_DATETIME=0
-SET GIT_BRANCH="LOCAL"
 
 SET VerMajor=0
 SET VerMinor=0
 SET VerBuild=0
+
+SET INT_NAME=0
 
 SET PN=0
 SET VS=0
@@ -30,23 +29,21 @@ SET PCF=0
 SET PYS=0
 SET PA=0
 
-SET INT_NAME=0
-
-CD /d %~dp0
+SET CURRENT_PATH=%CD%
+CD ..
+SET PARENT=!CURRENT_PATH:%CD%\=!
+SET NEW_PATH=%CURRENT_PATH%\%PARENT%
+CD /d "%NEW_PATH%"
+ECHO #pragma once > revision.h
 
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MAJOR" "Version.h"') DO (SET "VerMajor=%%A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MINOR" "Version.h"') DO (SET "VerMinor=%%A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_BUILD" "Version.h"') DO (SET "VerBuild=%%A")
-
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_NAME" "Version.h"') DO (SET "PN=%%~A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_YEAR_START" "Version.h"') DO (SET "PYS=%%A")
 FOR /F "tokens=2*" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_AUTHORS" "Version.h"') DO (SET "PA=%%~B")
-
 FOR /F "tokens=1-4 delims=:., " %%A IN ("%BUILDTIME%") DO (SET /A "BUILDSECS=%%A * 3600 + %%B * 60 + %%C")
-
-FOR /F "delims=" %%A IN ('git symbolic-ref --short HEAD') DO (SET GIT_BRANCH=%%A)
 FOR /F "delims=" %%A IN ('git rev-list --count HEAD') DO (SET /A GIT_COUNT=%%A)
-
 ::FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%y%%m%%d ^| find /I "Date:"') DO (SET GIT_DATE_NUM=%%B)
 FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%d.%%m.%%Y ^| find /I "Date:"') DO (SET "GIT_DATE=%%B")
 FOR /F "tokens=2-4 delims=, " %%A IN ('git log -1 --date=format:"%%a,%%d-%%h-%%Y,%%T" ^| find /I "Date:"') DO (
@@ -71,7 +68,6 @@ ECHO #define BUILD_TIME "%BUILDTIME%" >> revision.h
 ECHO #define GIT_DATE "%GIT_DATE%" >> revision.h
 ECHO #define GIT_TIME "%GIT_TIME%" >> revision.h
 ECHO #define GIT_DATETIME "%GIT_DATETIME%" >> revision.h
-ECHO #define GIT_BRANCH "%GIT_BRANCH%" >> revision.h
 ECHO #define GIT_COUNT %GIT_COUNT% >> revision.h
 ECHO #define V_SECS %BUILDSECS% >> revision.h
 ECHO #define INTERNAL_NAME %INT_NAME% >> revision.h
@@ -82,6 +78,6 @@ ECHO #define VERSION_NUM %VN% >> revision.h
 ECHO #define VERSION_STR "%VS%" >> revision.h
 ECHO #define VERSION_NUM_FULL %VNF% >> revision.h
 ECHO #define VERSION_STR_FULL "%VSF%" >> revision.h
-::pause
+pause
 ENDLOCAL
 EXIT

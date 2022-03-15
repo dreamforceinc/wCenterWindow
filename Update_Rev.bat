146208bd031f3bffb5a9404584eceedacd965646
W0LF <dreamforce@yandex.ru>
2022-03-15 17:31:09 +0300

    Update versioning.

--- Update_Rev.bat
+++ Update_Rev.bat
@@ -1,11 +1,13 @@
-::@ECHO OFF
+@ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
+SET CURRENT_PATH=%~dp0
 
 SET CURRENT_TIME=%TIME%
 SET CURRENT_DATE=%DATE%
 
 SET BUILDTIME=%CURRENT_TIME:~0,8%
 SET BUILDDATE=%CURRENT_DATE%
+SET BUILD_DATETIME=Build time: %BUILDDATE% %BUILDTIME%
 SET CURRENT_YEAR=%CURRENT_DATE:~6,4%
 
 SET BUILDSECS=0
@@ -29,12 +31,8 @@ SET PCF=0
 SET PYS=0
 SET PA=0
 
-SET CURRENT_PATH=%CD%
-CD ..
-SET PARENT=!CURRENT_PATH:%CD%\=!
-SET NEW_PATH=%CURRENT_PATH%\%PARENT%
-CD /d "%NEW_PATH%"
-ECHO #pragma once > revision.h
+CD /D "%CURRENT_PATH%"
+ECHO #pragma once> revision.h
 
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MAJOR" "Version.h"') DO (SET "VerMajor=%%A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MINOR" "Version.h"') DO (SET "VerMinor=%%A")
@@ -47,7 +45,7 @@ FOR /F "delims=" %%A IN ('git rev-list --count HEAD') DO (SET /A GIT_COUNT=%%A)
 ::FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%y%%m%%d ^| find /I "Date:"') DO (SET GIT_DATE_NUM=%%B)
 FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%d.%%m.%%Y ^| find /I "Date:"') DO (SET "GIT_DATE=%%B")
 FOR /F "tokens=2-4 delims=, " %%A IN ('git log -1 --date=format:"%%a,%%d-%%h-%%Y,%%T" ^| find /I "Date:"') DO (
-  SET "GIT_DATETIME=Build time: %%A, %%B %%C"
+  SET "GIT_DATETIME=Git time: %%A, %%B %%C"
   SET "GIT_TIME=%%C"
 )
 
@@ -56,28 +54,28 @@ SET VS=%VerMajor%.%VerMinor%.%VerBuild%
 SET VNF=%VerMajor%,%VerMinor%,%VerBuild%,%GIT_COUNT%
 SET VN=%VerMajor%,%VerMinor%,%VerBuild%
 
-SET PNF="%PN% v%VS% (C++)"
-SET PCF="Copyright (C) %PYS%-%CURRENT_YEAR% by %PA%"
+SET PNF=%PN% v%VS% (C++)
+SET PCF=Copyright (C) %PYS%-%CURRENT_YEAR% by %PA%
 
-SET INT_NAME="%PN%C++"
-SET ORIG_NAME="%PN%.exe"
+SET INT_NAME=%PN%C++
+SET ORIG_NAME=%PN%.exe
 
-ECHO #define BUILD_DATE "%BUILDDATE%" >> revision.h
-ECHO #define BUILD_TIME "%BUILDTIME%" >> revision.h
-::ECHO #define GIT_DATE_HUMAN "%GIT_DATE_NUM%" >> revision.h
-ECHO #define GIT_DATE "%GIT_DATE%" >> revision.h
-ECHO #define GIT_TIME "%GIT_TIME%" >> revision.h
-ECHO #define GIT_DATETIME "%GIT_DATETIME%" >> revision.h
+ECHO #define BUILD_DATE "%BUILDDATE%">> revision.h
+ECHO #define BUILD_TIME "%BUILDTIME%">> revision.h
+ECHO #define BUILD_DATETIME "%BUILD_DATETIME%">> revision.h
+ECHO #define GIT_DATE "%GIT_DATE%">> revision.h
+ECHO #define GIT_TIME "%GIT_TIME%">> revision.h
+ECHO #define GIT_DATETIME "%GIT_DATETIME%">> revision.h
 ECHO #define GIT_COUNT %GIT_COUNT% >> revision.h
 ECHO #define V_SECS %BUILDSECS% >> revision.h
-ECHO #define INTERNAL_NAME %INT_NAME% >> revision.h
-ECHO #define ORIG_FILE_NAME %ORIG_NAME% >> revision.h
-ECHO #define PRODUCT_NAME_FULL %PNF% >> revision.h
-ECHO #define PRODUCT_COPYRIGHT %PCF% >> revision.h
+ECHO #define INTERNAL_NAME "%INT_NAME%">> revision.h
+ECHO #define ORIG_FILE_NAME "%ORIG_NAME%">> revision.h
+ECHO #define PRODUCT_NAME_FULL "%PNF%">> revision.h
+ECHO #define PRODUCT_COPYRIGHT "%PCF%">> revision.h
 ECHO #define VERSION_NUM %VN% >> revision.h
-ECHO #define VERSION_STR "%VS%" >> revision.h
+ECHO #define VERSION_STR "%VS%">> revision.h
 ECHO #define VERSION_NUM_FULL %VNF% >> revision.h
-ECHO #define VERSION_STR_FULL "%VSF%" >> revision.h
-pause
+ECHO #define VERSION_STR_FULL "%VSF%">> revision.h
+
 ENDLOCAL
 EXIT

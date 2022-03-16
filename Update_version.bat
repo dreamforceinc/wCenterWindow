1929e3dc4fcd0ec02ef13b452be5543c76d1ae95
W0LF <dreamforce@yandex.ru>
2022-03-16 17:22:51 +0300

    Update versioning.

--- Update_version.bat
+++ Update_version.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-SET CURRENT_PATH=%~dp0
 
 SET CURRENT_TIME=%TIME%
 SET CURRENT_DATE=%DATE%
@@ -14,50 +13,46 @@ SET BUILDSECS=0
 SET GIT_COUNT=0
 SET GIT_TIME=0
 SET GIT_DATE=0
-SET GIT_DATE_NUM=0
 SET GIT_DATETIME=0
 
 SET VerMajor=0
 SET VerMinor=0
-SET VerBuild=0
+SET VerPatch=0
 
 SET INT_NAME=0
-
 SET PN=0
 SET VS=0
 SET VSF=0
-
 SET PCF=0
 SET PYS=0
 SET PA=0
 
-CD /D "%CURRENT_PATH%"
+CD /D %~dp0
 IF NOT EXIST "VersionInfo.h" (
 	ECHO Can't find file 'VersionInfo.h'
-	TIMEOUT /T 1
+	TIMEOUT /T 3
 	EXIT /B 1
 )
 COPY /Y "VersionInfo.h" "version.h" >nul
 
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MAJOR" "version.h"') DO (SET "VerMajor=%%A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_MINOR" "version.h"') DO (SET "VerMinor=%%A")
-FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_BUILD" "version.h"') DO (SET "VerBuild=%%A")
+FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define V_PATCH" "version.h"') DO (SET "VerPatch=%%A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_NAME" "version.h"') DO (SET "PN=%%~A")
 FOR /F "tokens=3" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_YEAR_START" "version.h"') DO (SET "PYS=%%A")
 FOR /F "tokens=2*" %%A IN ('FINDSTR /I /L /C:"define PRODUCT_AUTHORS" "version.h"') DO (SET "PA=%%~B")
 FOR /F "tokens=1-4 delims=:., " %%A IN ("%BUILDTIME%") DO (SET /A "BUILDSECS=%%A * 3600 + %%B * 60 + %%C")
 FOR /F "delims=" %%A IN ('git rev-list --count HEAD') DO (SET /A GIT_COUNT=%%A)
-::FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%y%%m%%d ^| find /I "Date:"') DO (SET GIT_DATE_NUM=%%B)
 FOR /F "tokens=1,2 delims= " %%A IN ('git log -1 --date=format:%%d.%%m.%%Y ^| find /I "Date:"') DO (SET "GIT_DATE=%%B")
 FOR /F "tokens=2-4 delims=, " %%A IN ('git log -1 --date=format:"%%a,%%d-%%h-%%Y,%%T" ^| find /I "Date:"') DO (
-  SET "GIT_DATETIME=Git time: %%A, %%B %%C"
-  SET "GIT_TIME=%%C"
+	SET "GIT_DATETIME=Git time: %%A, %%B %%C"
+	SET "GIT_TIME=%%C"
 )
 
-SET VSF=%VerMajor%.%VerMinor%.%VerBuild%.%GIT_COUNT%
-SET VS=%VerMajor%.%VerMinor%.%VerBuild%
-SET VNF=%VerMajor%,%VerMinor%,%VerBuild%,%GIT_COUNT%
-SET VN=%VerMajor%,%VerMinor%,%VerBuild%
+SET VSF=%VerMajor%.%VerMinor%.%VerPatch%.%GIT_COUNT%
+SET VS=%VerMajor%.%VerMinor%.%VerPatch%
+SET VNF=%VerMajor%,%VerMinor%,%VerPatch%,%GIT_COUNT%
+SET VN=%VerMajor%,%VerMinor%,%VerPatch%
 
 SET PNF=%PN% v%VS% (C++)
 SET PCF=Copyright (C) %PYS%-%CURRENT_YEAR% by %PA%

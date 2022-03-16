cc2e5b565441a102855d7b484a8fce0082ab5c19
W0LF <dreamforce@yandex.ru>
2022-03-16 17:42:44 +0300

    Released new version.

--- VersionInfo.h
+++ VersionInfo.h
@@ -5,7 +5,7 @@
 
 #define V_MAJOR				2
 #define V_MINOR				3
-#define V_PATCH				3
+#define V_PATCH				4
 #define PRODUCT_NAME		"wCenterWindow"
 #define PRODUCT_AUTHORS		"W0LF aka 'dreamforce'"
 #define PRODUCT_YEAR_START	2020

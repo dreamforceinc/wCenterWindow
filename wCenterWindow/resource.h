62ae981ff09b2eced0c6755d5b78b16a228fba32
dreamforceinc <dreamforce@yandex.ru>
2024-03-03 14:10:40 +0300

    Removed old About dialog from resources.

--- wCenterWindow/resource.h
+++ wCenterWindow/resource.h
@@ -2,48 +2,38 @@
 // Microsoft Visual C++ generated include file.
 // Used by wCenterWindow.rc
 //
-#define IDS_CLASSNAME                   102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    104
-#define IDI_TRAYICON                    105
-#define IDR_MENU                        106
-#define ID_POPUPMENU_ICON               107
-#define ID_POPUPMENU_AREA               108
-#define ID_POPUPMENU_HELP               109
-#define ID_POPUPMENU_ABOUT              110
-#define ID_POPUPMENU_EXIT               111
-#define IDS_HELP                        112
-#define IDS_ERR_MAIN                    113
-#define IDS_ERR_CLASS                   114
-#define IDS_ERR_WND                     115
-#define IDS_ERR_ICON                    116
-#define IDS_ERR_MENU                    117
-#define IDS_ERR_POPUP                   118
-#define IDS_ERR_HOOK                    119
-#define IDS_ERR_TIMER                   120
-#define IDS_ERR_HEAP                    121
-#define IDS_ERR_MAXMIN                  122
-#define IDS_RUNNING                     123
-#define IDT_TIMER                       124
-#define IDR_MAINFRAME                   128
-#define IDD_MANUAL_EDITING              129
-#define IDC_EDIT_X                      1000
-#define IDC_EDIT_Y                      1001
-#define IDC_EDIT_WIDTH                  1002
-#define IDC_EDIT_HEIGHT                 1003
-#define IDC_EDIT_TITLE                  1004
-#define IDC_EDIT_CLASS                  1005
-#define IDC_BUTTON_SET                  1006
-#define IDC_BUTTON_CENTER               1007
-#define IDC_BUTTON_CLOSE                1008
-#define IDC_ABOUTHELP                   1009
-#define IDC_DONATIONLINK                1010
-#define IDC_DONATIONTEXT                1011
-#define IDC_ABOUT_PROGNAME              1012
-#define IDC_ABOUT_COPYRIGHT             1013
-#define IDC_ABOUT_BUILDTIME             1014
-#define IDC_ABOUTEDIT                   1015
-#define IDC_ABOUT_COPYRIGHT_ICON        1016
+#define IDS_CLASSNAME                   100
+#define IDS_APP_TITLE                   101
+#define IDI_TRAYICON                    102
+#define IDR_MENU                        103
+#define ID_POPUPMENU_ICON               104
+#define ID_POPUPMENU_AREA               105
+#define ID_POPUPMENU_HELP               106
+#define ID_POPUPMENU_ABOUT              107
+#define ID_POPUPMENU_EXIT               108
+#define IDS_HELP                        109
+#define IDS_ERR_MAIN                    110
+#define IDS_ERR_CLASS                   111
+#define IDS_ERR_WND                     112
+#define IDS_ERR_ICON                    113
+#define IDS_ERR_MENU                    114
+#define IDS_ERR_POPUP                   115
+#define IDS_ERR_HOOK                    116
+#define IDS_ERR_TIMER                   117
+#define IDS_ERR_HEAP                    118
+#define IDS_ERR_MAXMIN                  119
+#define IDS_RUNNING                     120
+#define IDT_TIMER                       121
+#define IDD_MANUAL_EDITING              1000
+#define IDC_EDIT_X                      1001
+#define IDC_EDIT_Y                      1002
+#define IDC_EDIT_WIDTH                  1003
+#define IDC_EDIT_HEIGHT                 1004
+#define IDC_EDIT_TITLE                  1005
+#define IDC_EDIT_CLASS                  1006
+#define IDC_BUTTON_SET                  1007
+#define IDC_BUTTON_CENTER               1008
+#define IDC_BUTTON_CLOSE                1009
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -51,9 +41,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        200
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1016
-#define _APS_NEXT_SYMED_VALUE           125
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           122
 #endif
 #endif
